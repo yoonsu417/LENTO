7fbec0371133785874c7352eb985eea22602909b
YOONSU <yoonsu1717@gmail.com>
2024-04-02 21:32:49 +0900

    로그인 화면 틀, 회원가입 화면 틀, 로그인 -> 회원가입 이동

--- app/src/main/java/com/example/lento/MainActivity.java
+++ app/src/main/java/com/example/lento/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.lento;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static void main(String[] args) {
-        System.out.println("test 코드");
-    }
+    EditText InputEmail, InputPw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        //이메일, 비밀번호 입력
+        InputEmail = (EditText) findViewById(R.id.InputEmail);
+        InputPw = (EditText) findViewById(R.id.InputPw);
+
+
+        // 계정이 없으신가요? 눌렀을 때 회원가입 화면으로 이동
+        TextView NoAccount;
+        NoAccount = (TextView) findViewById(R.id.account);
+        NoAccount.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                setContentView(R.layout.activity_join);
+            }
+        });
+
+
     }
 }
