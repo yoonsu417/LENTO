633eece1eaa881c2b09af5eb221047f687953ba2
YOONSU <yoonsu1717@gmail.com>
2024-04-04 21:22:08 +0900

    악보 추가 화면

--- app/src/main/java/com/example/lento/HomeActivity.java
+++ app/src/main/java/com/example/lento/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.lento;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class HomeActivity extends Activity {
@@ -11,6 +14,16 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 악보이미지 누르면 악보 추가
+        //로그인 누르면 홈 화면으로 이동
+        Button Upload;
+        Upload = (Button)findViewById(R.id.Upload);
+        Upload.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, UploadActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
