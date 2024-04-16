683d3edb01bbbe8e7eb8291578eff48ebfc1dbe7
YOONSU <yoonsu1717@gmail.com>
2024-04-17 01:41:44 +0900

    코드 합치기

--- app/src/main/java/com/example/lento/OpenCVtestActivity.java
+++ app/src/main/java/com/example/lento/OpenCVtestActivity.java
@@ -41,11 +41,6 @@ public class OpenCVtestActivity extends AppCompatActivity {
         ImageView OpenCVtest;
         OpenCVtest = findViewById(R.id.OpenCVtest);
 
-        // 출력할 비트맵 객체 선언
-        Bitmap Bitmapimage;
-        OpenCVLoader.initDebug();
-
-
         // Mat 객체 불러오기 : JPG -> Mat
         Mat image = null;
         try {
@@ -60,23 +55,84 @@ public class OpenCVtestActivity extends AppCompatActivity {
             return;
         }
 
-        Mat grayMat = new Mat();
-        Mat binaryMat = new Mat();
-
-        // 그레이스케일
-        Imgproc.cvtColor(image, grayMat, Imgproc.COLOR_BGR2GRAY);
+        // 그레이 스케일
+        Mat grayImage = new Mat();
+        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
 
         // 이진화
-        Imgproc.threshold(grayMat, binaryMat, 0, 255, Imgproc.THRESH_BINARY_INV | Imgproc.THRESH_OTSU);
+        Mat binaryImage = new Mat();
+        Imgproc.threshold(grayImage, binaryImage, 0, 255, Imgproc.THRESH_BINARY_INV | Imgproc.THRESH_OTSU);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(binaryImage, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // 보표로 추정되는 영역을 표시할 이미지 생성
+        Mat extractedImage = Mat.zeros(binaryImage.size(), binaryImage.type());
+
+        for (MatOfPoint contour : contours) {
+            // 컨투어를 감싸는 최소 사각형(rectangle)을 구함
+            Rect rect = Imgproc.boundingRect(contour);
+            // 최소 사각형의 가로 길이가 이미지 가로 길이의 70% 이상이면 보표로 판단하고 표시할 이미지에 해당 부분을 그림
+            if (rect.width >= binaryImage.cols() * 0.7 && rect.width != binaryImage.cols()) {
+                // 보표로 판단된 영역을 표시할 이미지에 그림
+                Imgproc.rectangle(extractedImage, rect.tl(), rect.br(), new Scalar(255, 255, 255), -1); // 흰색으로 채움
+            }
+        }
+
+        // 추출된 보표 영역만을 남기고 나머지 부분을 검정색으로 만듬
+        Core.bitwise_and(binaryImage, extractedImage, binaryImage);
+
+        // 오선 제거
+        removeStaves(binaryImage);
 
         // 비트맵 선언 + Mat 객체 -> 비트맵 변환
-        Bitmapimage = Bitmap.createBitmap(binaryMat.cols(), binaryMat.rows(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(binaryMat, Bitmapimage);
+        Bitmap Bitmapimage;
+        Bitmapimage = Bitmap.createBitmap(binaryImage.cols(), binaryImage.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(binaryImage, Bitmapimage);
 
         // 비트맵 이미지 화면 출력
         OpenCVtest.setImageBitmap(Bitmapimage);
 
     }
 
+    // 수평 히스토그램을 사용하여 오선을 삭제하는 메서드
+    public static Mat removeStaves(Mat image) {
+        int height = image.rows();
+        int width = image.cols();
+        List<int[]> staves = new ArrayList<>();
+
+        // 오선 검출
+        for (int row = 0; row < height; row++) {
+            int pixels = 0;
+            for (int col = 0; col < width; col++) {
+                if (image.get(row, col)[0] == 255) { // 흰색 픽셀 개수 세기
+                    pixels++;
+                }
+            }
+            if (pixels >= width * 0.5) { // 이미지 너비의 50% 이상이라면
+                if (staves.isEmpty() || Math.abs(staves.get(staves.size() - 1)[0] + staves.get(staves.size() - 1)[1] - row) > 1) { // 첫 오선이거나 이전에 검출된 오선과 다른 오선
+                    staves.add(new int[] {row, 0}); // 오선 추가 [오선의 y 좌표][오선 높이]
+                } else { // 이전에 검출된 오선과 같은 오선
+                    staves.get(staves.size() - 1)[1]++; // 높이 업데이트
+                }
+            }
+        }
+
+        // 오선 제거
+        for (int[] staff : staves) {
+            int topPixel = staff[0]; // 오선의 최상단 y 좌표
+            int botPixel = staff[0] + staff[1]; // 오선의 최하단 y 좌표 (오선의 최상단 y 좌표 + 오선 높이)
+            for (int col = 0; col < width; col++) {
+                if (image.get(topPixel - 1, col)[0] == 0 && image.get(botPixel + 1, col)[0] == 0) { // 오선 위, 아래로 픽셀이 있는지 탐색
+                    for (int row = topPixel; row <= botPixel; row++) {
+                        image.put(row, col, 0); // 오선을 지움
+                    }
+                }
+            }
+        }
+
+        return image;
+    }
 
 }
